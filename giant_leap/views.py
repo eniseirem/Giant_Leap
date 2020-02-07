0845976ae8ea2bbac8e95bfa037ba5da4db59d77
eniseirem <eniseirem@gmail.com>
2020-02-07 22:52:21 +0300

    Created first page layout added search bar

--- giant_leap/views.py
+++ giant_leap/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+import requests
+import json
 
-# Create your views here.
+
+key = "?api_key=V7RttlrR5x2ul8TvNpLCXo0CXNOvVS8VAw4HbkmT"
+
+def homepage(request):
+    response = requests.get('https://api.nasa.gov/planetary/apod'+key)
+    apodata = response.json()
+
+    return render(request, 'home.html', {'pics': apodata})
